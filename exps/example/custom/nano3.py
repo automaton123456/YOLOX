9d9c5d8147824ca5bd4c59ec15161016d4601486
automaton123456 <30631788+automaton123456@users.noreply.github.com>
2021-09-20 10:46:04 +0100

    Update nano3.py

--- exps/example/custom/nano3.py
+++ exps/example/custom/nano3.py
@@ -32,6 +32,8 @@ class Exp(MyExp):
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
         
         self.num_classes = 5
+        
+        self.multiscale_range = 0
 
     def get_model(self, sublinear=False):
         def init_yolo(M):
